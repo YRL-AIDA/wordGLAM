de43c2b9cf7a1f28683fc600a76611d250e54999
Dann38 <38dunn@gmail.com>
2025-07-21 09:38:43 +0800

    new exp_rows

---
+++ exp_06_rows/exp_1/__init__.py
@@ -0,0 +1,8 @@
+
+import sys, os
+sys.path.append("..")
+from exp_06_rows import *
+
+EXPERIMENT_PARAMS["loss_params"]["edge_coef"] = 0.1
+EXPERIMENT_PARAMS["loss_params"]["row_coef"] = 0.8
+EXPERIMENT_PARAMS["loss_params"]["node_coef"] = 0.1
---
+++ exp_06_rows/start_more_exp.py
@@ -0,0 +1,52 @@
+import subprocess
+import os
+
+HEADER = "exp_06_rows"
+
+sizes = [
+    [0.1, 0.1, 0.8],
+    [0.1, 0.8, 0.1],
+    [0.8, 0.1, 0.1],
+    [0.2, 0.2, 0.6],
+    [0.2, 0.6, 0.2],
+    [0.6, 0.2, 0.2],
+    [0.3, 0.3, 0.4],
+    [0.3, 0.4, 0.3],
+    [0.4, 0.3, 0.3]
+]
+
+def created_exps():
+    
+    for i in range(0, 9):
+        dir_ = os.path.join(HEADER, f"exp_{i}")
+        os.mkdir(dir_)
+        file_ = os.path.join(dir_, "__init__.py")
+        with open(file_, 'w') as f:
+            f.write(f"""
+import sys, os
+sys.path.append("..")
+from exp_06_rows import *
+
+EXPERIMENT_PARAMS["loss_params"]["edge_coef"] = {sizes[i][0]}
+EXPERIMENT_PARAMS["loss_params"]["row_coef"] = {sizes[i][1]}
+EXPERIMENT_PARAMS["loss_params"]["node_coef"] = {sizes[i][2]}
+"""
+)
+def start_new_exp(name, header=HEADER):
+    with open ('.env', 'r') as f:
+        old_data = f.read()
+    new_data = "\n".join([f'EXPERIMENT="{header}/{name}"']+old_data.split("\n")[1:])
+    with open ('.env', 'w') as f:
+        f.write(new_data)
+    
+
+    subprocess.run(["python", "script_train.py"])
+    subprocess.run(["python", "script_test.py"])
+
+
+if not os.path.exists(os.path.join(HEADER, "exp_2_kTag_2")):
+    created_exps()
+for i in range(0, 9):
+    dir_ = os.path.join(HEADER, f"exp_{i}", "test_result.txt")
+    if not os.path.exists(dir_):
+        start_new_exp(f"exp_{i}")
